54304443a64e57e9212c8f7e1fa555127bf6cdf0
Nicholas Gong <gongzkong@gmail.com>
2023-10-03 14:04:19 -0700

    added papa parse package and reconfigure packages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 //import logo from './logo.svg';
 import sanitationLogo from './cityLA-logo-website.jpg' //you have to import using this logo import for react
 import './App.css';
+import { GithubLogo } from './GithubLogo';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <GithubLogo/>
         <img src={sanitationLogo} className="App-logo" alt="lasan-logo.jpg" />
 
-        <h1>This is Nick's ReactApp LASAN react.</h1>
+        <h1>LASAN Robocaller Csv Parser react.</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+    
         <a
           className="App-link"
           href="https://reactjs.org"
