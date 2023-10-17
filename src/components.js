85200deb4fb669e83ae0566e6a2365b3cfcf046e
Nicholas Gong <gongzkong@gmail.com>
2023-10-17 16:34:07 -0700

    update to handle xlsx file type uploads

--- src/components.js
+++ src/components.js
@@ -1,82 +1,121 @@
 
 import {useState} from "react";
-import Papa from 'papaparse'; //import Papaprase
+import Papa from "papaparse"; //import Papaprase
+import * as XLSX from "xlsx";
 
 //First parser component
 export const Parser = () => {
   //useState definitions here
-    const defualtState = [{
+    const defaultState = [{
       TelephoneNumber: '1234567890', ContactAttempts: 0, FullMessage: 0, lastCallBlast: 0, lastCallBlastTime: 0,
-      Name: 'Constiuent', SuccessfulConnection: 0, ToVoiceMail:0, Route: 'Default', District:'ICSD'
-    }];
+      Name: 'Constiuent', SuccessfulConnection: 0, ToVoiceMail:0, Route: 'Default', District:'ICSD'}];
+    const intialFileTypeState = ['Please upload a file'];
 
-    const [data, setData] = useState(defualtState);
+    const [data, setData] = useState(defaultState);
+    const [fileType, setFileType] = useState(intialFileTypeState);
 
+    //debugging method
     function consoleLogFile() {
       console.log(data);
       }
 
-
     //https://stackoverflow.com/questions/67950444/how-to-convert-csv-file-data-to-json-object-in-reactjs
-    function handleFileUpload(event){
-      const files = event.target.files;
+    const handleFileUpload = async(event) => {
+      const files = event.target.files,
+            fileName = files[0] ? files[0].name : 'No file uploaded',
+            fileExtensionType = (fileName.match(/\.csv|\.xlsx|.xls/gi) || [])[0];
 
+            //thes check file input
+            // console.log(files)
+            // console.log(fileName)
+            // console.log(fileExtensionType)
+
+      if(fileExtensionType === '.csv') setFileType('Commodity csv');
+      if(fileExtensionType === '.xlsx') setFileType('Bulky xlsx');
+      if(fileExtensionType === '.xls') setFileType('Bulky xls');
+      
       if (files.length === 0){
-        setData(defualtState);
+        setData(defaultState);
+        setFileType(intialFileTypeState);
       }
-
+      //processes data using Papaparse based on Commodity csv file
       if(files.length > 0){
-        Papa.parse(files[0], {
-          header: true,
-          complete: function(results) {
-            const incommingData = results.data,
-                  convertedData = incommingData.map(dataObject => {
-                    const entries = Object.entries(dataObject),
-                          name = entries.filter(x => x[0] === 'PREM_ID').map(index => ['Name', index[1]]),
-                          phone = entries.filter(x => x[0] === 'PHONE').map(index => ['TelephoneNumber', index[1].replace(/-/gi,'')]),
-                          route = entries.filter(x => x[0] === 'ROUTES').map(index => ['Route', index[1]]),
-                          district = entries.filter(x => x[0] === 'DISTRICT_NAME').map(index => ['District', index[1]]),
-                          convertedEntries = Object.fromEntries([name, phone, route, district].flat());
-                    
-                    //set intial object state again
-                    convertedEntries.ContactAttempts = 0;
-                    convertedEntries.FullMessage = 0;
-                    convertedEntries.lastCallBlast = 0;
-                    convertedEntries.lastCallBlastTime = 0;
-                    convertedEntries.SuccessfulConnection = 0;
-                    convertedEntries.ToVoiceMail = 0;
+        if(fileExtensionType === '.csv'){
+          Papa.parse(files[0], {
+            header: true,
+            complete: function(results) {
+              const incommingCommodityData = results.data
+              const convertedCommodityData = incommingCommodityData.map(dataObject => {
 
-                    //return final array of correctly filled/populated objects
-                    return convertedEntries;
-                  });
+                const entries = Object.entries(dataObject),
+                      name = entries.filter(x => x[0] === 'PREM_ID').map(index => ['Name', index[1]]),
+                      phone = entries.filter(x => x[0] === 'PHONE').map(index => ['TelephoneNumber', index[1].replace(/-/gi,'')]),
+                      route = entries.filter(x => x[0] === 'ROUTES').map(index => ['Route', index[1]]),
+                      district = entries.filter(x => x[0] === 'DISTRICT_NAME').map(index => ['District', index[1]]),
+                      convertedEntries = Object.fromEntries([name, phone, route, district].flat());
+                
+                //set intial object state again
+                convertedEntries.ContactAttempts = 0;
+                convertedEntries.FullMessage = 0;
+                convertedEntries.lastCallBlast = 0;
+                convertedEntries.lastCallBlastTime = 0;
+                convertedEntries.SuccessfulConnection = 0;
+                convertedEntries.ToVoiceMail = 0;
 
-                  //removes last row due to intial values being re-set
-                  const cleandData = convertedData.slice(0, convertedData.length-1)
-
-
-            //uncomment below for debugging
-            //console.log('Parsed Data here:', incommingData)
-            //console.log(convertedDate)
-
-
-            
-            setData(cleandData)
-          }
+                //return final array of correctly filled/populated objects
+                return convertedEntries;
+              });
+                //removes last row due to intial values being re-set
+                const cleandCommodityData = convertedCommodityData.slice(0, convertedCommodityData.length-1);
+                setData(cleandCommodityData)
+            }
+          })
         }
-        )
-      }
+        //processes data using XLSX from SheetJS based on Bulky xlsx file
+        if(fileExtensionType === '.xlsx'){
+          const data = await files[0].arrayBuffer(),
+                workbook = XLSX.read(data),
+                worksheet = workbook.Sheets[workbook.SheetNames[0]],
+                jsonData = XLSX.utils.sheet_to_json(worksheet);
 
+          const convertedBulkyData = jsonData.map(dataObject => {
+            const entries = Object.entries(dataObject),
+                  name = entries.filter(x => x[0] === 'SRContactName').map(index => ['Name', index[1]]),
+                  phone = entries.filter(x => x[0] === 'SRContactPhone').map(index => ['TelephoneNumber', typeof index[1] === 'string' ? 
+                                                                                                            index[1].replace(/-/gi,'') : index[1].toString()]),
+                  convertedBulkyEntries = Object.fromEntries([name, phone].flat());
+            
+            //set intial object state again
+            convertedBulkyEntries.ContactAttempts = 0;
+            convertedBulkyEntries.FullMessage = 0;
+            convertedBulkyEntries.lastCallBlast = 0;
+            convertedBulkyEntries.lastCallBlastTime = 0;
+            convertedBulkyEntries.SuccessfulConnection = 0;
+            convertedBulkyEntries.ToVoiceMail = 0;
+            convertedBulkyEntries.Route = 'N/A';
+            convertedBulkyEntries.District = 'N/A';
+
+            //return final array of correctly filled/populated objects
+            return convertedBulkyEntries;
+          });
+            //removes last row due to intial values being re-set
+            const cleandBulkyData = convertedBulkyData.slice(0, convertedBulkyData.length-1);
+            setData(cleandBulkyData)
+          }
     }
+  }
 
     return (
         <div>
             <input type="file" id="input-file" accept=".csv,.xlsx,.xls"
-             onChange = {handleFileUpload}
+             onChange = {(e) => handleFileUpload(e)}
             />
           {/* This button is for debugging */}
-          <button className="button1" onClick={consoleLogFile}>
+          <button className="button1" id='displayDataButton' onClick={consoleLogFile}>
             Display Data
           </button>
+          <p> File type: {fileType}
+          </p>
             <table>
               <thead>
                 <tr>
